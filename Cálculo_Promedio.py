25c89913666765a2a846d9678f4ee3a67421f9d5
cristian <cristianariel.sanhueza@gmail.com>
2021-08-12 15:53:19 -0300

    actualización del programa

---
+++ Cálculo_Promedio.py
@@ -0,0 +1,8 @@
+def prom (a,b,c):
+    return (a+b+c)/3
+
+n1 = float(input("ingrese nota 1: "))
+n2 = float(input("ingrese nota 2: "))
+n3 = float(input("ingrese nota 3: "))
+
+print(f"El promedio de notas es de {prom(n1,n2,n3)}")
